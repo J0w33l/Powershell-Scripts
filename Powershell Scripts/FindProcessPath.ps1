3b9d27dffa7a388ab496030062f5bfff5bd0b3f1
J0w33l <108775979+J0w33l@users.noreply.github.com>
2024-06-26 23:03:59 -0600

    Get Process Path
    Get Path of process with the PID

---
+++ Powershell Scripts/FindProcessPath.ps1
@@ -0,0 +1,33 @@
+﻿#Process Location
+function ValidPID {
+    param(
+        [string]$prompt = "Enter Process ID"
+    )
+
+    while ($true) {
+
+        $procPID = Read-Host $prompt
+        try {
+            $IntprocID = [int]$procPID
+            return $IntprocID
+        }
+        catch {
+            Write-Host "Put only Process ID numbers"
+        }
+
+    }
+
+}
+
+$ProcessPID = ValidPID
+
+Write-Host "Process ID is: $ProcessPID"
+
+try {
+    $process = Get-Process -id $ProcessPID -ErrorAction Stop
+    $processPath = $process.MainModule.FileName
+    Write-Output = "The path of the process $ProcessPID IS: $processPath" 
+}
+catch {
+    Write-Host "Error Generated, Check your PID"
+}
